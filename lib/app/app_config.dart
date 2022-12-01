122e12cca26aa2c3725133c3741ec3f1da7e1f57
sayedseu <sayedhasanseu@gmail.com>
2022-12-01 15:50:26 +0600

    pos platform plugin migrate for both usb and ble

---
+++ lib/app/app_config.dart
@@ -0,0 +1,9 @@
+class AppConfig {
+  static String appUserAgent = "";
+  static String appAgent = "";
+  static String appVersion = "106";
+  static String appVersionName = "";
+  static String paymentUrl = "";
+  static bool isOnline = true;
+  static int connectionPingTimer = 5000;
+}
