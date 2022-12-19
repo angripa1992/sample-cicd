df5e921b091a84beef957914e96a94fa8db7544d
sayedseu <sayedhasanseu@gmail.com>
2022-12-19 11:24:58 +0600

    staging base url point to live server

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -3,8 +3,10 @@ import 'package:klikit/main.dart';
 
 void main() {
   final environmentVariables = EnvironmentVariables(
-    baseUrl: 'https://api-qa.shadowchef.co',
-    cdnUrl: 'https://cdn-qa.shadowchef.co',
+    // baseUrl: 'https://api-qa.shadowchef.co',
+    // cdnUrl: 'https://cdn-qa.shadowchef.co',
+    baseUrl: 'https://gateway-enterprise.klikit.io',
+    cdnUrl: 'https://assets.klikit.io',
   );
   mainCommon(environmentVariables);
 }
