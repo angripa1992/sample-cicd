03bde7ec82ad087462b1dea8635a09d9a1fc7b52
sayedseu <sayedhasanseu@gmail.com>
2022-12-30 15:45:02 +0600

    account screen ui improve

---
+++ lib/modules/user/presentation/account/component/account_action_header.dart
@@ -0,0 +1,45 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:klikit/modules/user/presentation/account/component/account_action_item.dart';
+
+import '../../../../../core/route/routes.dart';
+import '../../../../../resources/strings.dart';
+
+class AccountActionHeader extends StatelessWidget {
+  const AccountActionHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        AccountActionItem(
+          title: AppStrings.change_language.tr(),
+          iconData: Icons.language,
+          onTap: () {
+          },
+        ),
+        AccountActionItem(
+          title: AppStrings.printer_settings.tr(),
+          iconData: Icons.print,
+          onTap: () {
+            Navigator.of(context).pushNamed(Routes.printerSettings);
+          },
+        ),
+        AccountActionItem(
+          title: AppStrings.contact_support.tr(),
+          iconData: Icons.support_agent,
+          onTap: () {
+            Navigator.of(context).pushNamed(Routes.contactSupport);
+          },
+        ),
+        AccountActionItem(
+          title: AppStrings.change_password.tr(),
+          iconData: Icons.lock,
+          onTap: () {
+            Navigator.of(context).pushNamed(Routes.changePassword);
+          },
+        ),
+      ],
+    );
+  }
+}
