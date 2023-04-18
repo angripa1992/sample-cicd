fbfa97c686b32308b4d9c6fd9e67329f28110774
sayedseu <sayedhasanseu@gmail.com>
2023-04-18 18:06:46 +0600

    printer font size and payment info added on docket complete

--- lib/printer/data/dockets_fonts.dart
+++ lib/printer/data/dockets_fonts.dart
@@ -6,11 +6,11 @@ class PrinterFontSize {
 }
 
 class SmallFontSize {
-  static const double small = 10;
-  static const double regular = 11;
-  static const double medium = 12;
-  static const double large = 16;
-  static const double extraLarge = 18;
+  static const double small = 8;
+  static const double regular = 9;
+  static const double medium = 10;
+  static const double large = 14;
+  static const double extraLarge = 16;
 }
 
 class NormalFontSize {
@@ -22,17 +22,17 @@ class NormalFontSize {
 }
 
 class LargeFontSize {
-  static const double small = 10;
-  static const double regular = 11;
-  static const double medium = 12;
-  static const double large = 16;
-  static const double extraLarge = 18;
+  static const double small = 12;
+  static const double regular = 13;
+  static const double medium = 14;
+  static const double large = 18;
+  static const double extraLarge = 20;
 }
 
 class ExtraLargeFontSize {
-  static const double small = 10;
-  static const double regular = 11;
-  static const double medium = 12;
-  static const double large = 16;
-  static const double extraLarge = 18;
+  static const double small = 14;
+  static const double regular = 15;
+  static const double medium = 16;
+  static const double large = 20;
+  static const double extraLarge = 22;
 }
