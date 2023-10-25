3311e5448f53c064ee1cf6b4becdcd7e87ff7a3d
sayedvivasoft <sayed.hasan@vivasoftltd.com>
2023-10-25 13:27:42 +0600

    on progress

--- lib/modules/orders/presentation/components/order_item/order_action_button_manager.dart
+++ lib/modules/orders/presentation/components/order_item/order_action_button_manager.dart
@@ -70,7 +70,7 @@ class OrderActionButtonManager {
   }
 
   bool canUpdateOrder(Order order) {
-    return order.providerId == ProviderID.KLIKIT && order.isManualOrder && (order.status == OrderStatus.ACCEPTED || order.status == OrderStatus.PLACED);
+    return order.providerId == ProviderID.KLIKIT && (order.status == OrderStatus.ACCEPTED || order.status == OrderStatus.PLACED) && order.canUpdate;
   }
 
   bool canPrint(Order order) {
