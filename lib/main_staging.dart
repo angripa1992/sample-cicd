f06e576cb04472165ded86284e3179796627c78f
sayedvivasoft <sayed.hasan@vivasoftltd.com>
2023-11-08 17:04:59 +0600

    drive thru test build

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -5,7 +5,7 @@ void main() {
   final environmentVariables = EnvironmentVariables(
     baseUrl: 'https://api-qa.shadowchef.co',
     cdnUrl: 'https://cdn-qa.shadowchef.co',
-    consumerUrl: 'https://consumer.dev.shadowchef.co',
+    consumerUrl: 'https://consumer.qa.shadowchef.co',
     segmentWriteKey: 'djROSUJqa21nc2dKUUJTOUJGaWU3ckFsZHIwM01nSGg=',
     slackUrl: 'https://hooks.slack.com/services/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',
     zohoAppKey: 'FlIXMZG3VUtYzlN8MUemAXC8RKrBKQds1Er4rWZQvv5GPkjAZYzxQ9OtSvBx7ai6',
