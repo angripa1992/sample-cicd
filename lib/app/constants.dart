5116bd320152f5a8fb7679bde77be0ee7592573a
fizz-viva <m.rahman@vivasoftltd.com>
2023-11-28 17:18:32 +0600

    MOB-87: Code reformat undone

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppConstant {
-  static const String signUpUrl =
-      "https://www.klikit.io/contact?__hstc=140989165.3e68213999864dc1ab0072336446cdf8.1660889383375.1661919284520.1661927239769.12&__hssc=140989165.1.1661927239769&__hsfp=2812450310";
+  static const String signUpUrl = "https://www.klikit.io/contact?__hstc=140989165.3e68213999864dc1ab0072336446cdf8.1660889383375.1661919284520.1661927239769.12&__hssc=140989165.1.1661927239769&__hsfp=2812450310";
   static const String roleBranchManger = "branch_manager";
   static const String roleAdmin = "admin";
   static const String roleBrandManager = "brand_manager";
