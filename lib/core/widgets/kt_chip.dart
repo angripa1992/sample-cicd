febdf6fd5ee1999d7645564a1eb240a881a1acf7
fizz-viva <m.rahman@vivasoftltd.com>
2023-12-20 17:12:59 +0600

    [ADD]: Few more components introduced

---
+++ lib/core/widgets/kt_chip.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:klikit/core/widgets/decorated_image_view.dart';
+import 'package:klikit/resources/colors.dart';
+
+class KTChip extends StatelessWidget {
+  final String text;
+  final double borderRadius;
+  final Color backgroundColor;
+  final EdgeInsets? padding;
+  final DecoratedImageView? leadingIcon;
+  final DecoratedImageView? trailingIcon;
+  final TextStyle? textStyle;
+
+  const KTChip({
+    Key? key,
+    required this.text,
+    required this.borderRadius,
+    required this.backgroundColor,
+    this.padding,
+    this.leadingIcon,
+    this.trailingIcon,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding ?? const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        border: Border.all(color: AppColors.neutralB40),
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (leadingIcon != null) (leadingIcon!),
+          if (leadingIcon != null) const SizedBox(width: 4),
+          Flexible(
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: textStyle,
+            ),
+          ),
+          if (trailingIcon != null) const SizedBox(width: 4),
+          if (trailingIcon != null) (trailingIcon!),
+        ],
+      ),
+    );
+  }
+}
