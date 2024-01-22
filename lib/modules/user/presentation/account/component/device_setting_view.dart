f95f1f8a38148c770bbfe49d8e56b226fed90fe5
angripa.nadapdap <angripa.nadapdap@klikit.io>
2024-01-22 11:24:41 +0700

    add device setting

--- lib/modules/user/presentation/account/component/device_setting_view.dart
+++ lib/modules/user/presentation/account/component/device_setting_view.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:klikit/app/app_preferences.dart';
 import 'package:klikit/app/constants.dart';
 import 'package:klikit/app/di.dart';
 import 'package:klikit/app/extensions.dart';
@@ -29,8 +28,13 @@ class DeviceSettingScreen extends StatefulWidget {
 class _DeviceSettingScreenState extends State<DeviceSettingScreen> {
   final _devices = [Device.android, Device.sunmi, Device.imin];
   int? _device;
-  final Map _devicesDetail = {Device.android: 'Android',Device.sunmi: 'Sunmi',Device.imin: 'Imin'};
-  final _updateButtonController = KTButtonController(label: AppStrings.update.tr());
+  final Map _devicesDetail = {
+    Device.android: 'Android',
+    Device.sunmi: 'Sunmi',
+    Device.imin: 'Imin'
+  };
+  final _updateButtonController =
+      KTButtonController(label: AppStrings.update.tr());
 
   @override
   void initState() {
@@ -64,9 +68,10 @@ class _DeviceSettingScreenState extends State<DeviceSettingScreen> {
                 contentPadding: EdgeInsets.fromLTRB(12.rw, 0, 8.rw, 0),
                 selected: _device == device,
                 selectedTileColor: AppColors.neutralB20,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.rSp)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.rSp)),
                 title: Text(
-                  device == Device.android ? 'Android' : 'Sunmi',
+                  _devicesDetail[device],
                   style: mediumTextStyle(
                     color: AppColors.black,
                     fontSize: AppFontSize.s14.rSp,
@@ -104,12 +109,15 @@ class _DeviceSettingScreenState extends State<DeviceSettingScreen> {
             builder: (context, state) {
               return KTButton(
                 controller: _updateButtonController,
-                backgroundDecoration: regularRoundedDecoration(backgroundColor: AppColors.primaryP300),
+                backgroundDecoration: regularRoundedDecoration(
+                    backgroundColor: AppColors.primaryP300),
                 labelStyle: mediumTextStyle(color: AppColors.white),
                 progressPrimaryColor: AppColors.white,
                 verticalContentPadding: 10.rh,
                 onTap: () {
-                  context.read<DeviceSettingCubit>().changeSunmiDeviceSetting(_device == Device.sunmi);
+                  context
+                      .read<DeviceSettingCubit>()
+                      .changeSunmiDeviceSetting(_device == Device.sunmi);
                 },
               );
             },
