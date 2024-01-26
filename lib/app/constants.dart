44b22d9fc7ef8e63c4ffc860523acf2a0ad644a3
fizz-viva <m.rahman@vivasoftltd.com>
2024-01-26 15:31:22 +0600

    MOB-138: Date time patterns are separated from constants file

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -234,23 +234,3 @@ class ArgumentKey {
   static const String kOPEN_CART = 'clear_cart';
   static const String kUPDATE_CART = 'update_order';
 }
-
-class DTPatterns {
-  static String dMMMyyyy = "d MMM yyyy";
-  static String edmmmyyyy = "E, d MMM yyyy";
-  static String d_mmm_yyyy = "d MMM yyyy";
-  static String edmmm = "E, d MMM";
-  static String ddmmyyyy = "dd-MM-yyyy";
-  static String ddmmyyyySplash = "dd/MM/yyyy";
-  static String yyyymmdd = "yyyy-MM-dd";
-  static String mmmyyyy = "MMM yyyy";
-  static String dmmm = "d MMM";
-  static String dmmmhhmma = "d MMM hh:mm a";
-  static String mmmm_comma_yyyy = "MMMM, yyyy";
-  static String dd_mm_yyyy_hh_mm_a = "dd-MM-yyyy hh:mm a";
-  static String dd_mm_yyyy_hh_mm_ss_a = "dd-MM-yyyy hh:mm:ss a";
-  static String edmmmyy = "E, d MMM yy";
-  static String hh_mm_ss_a = 'hh:mm:ss a';
-  static String hh_mm_a = 'hh:mm a';
-  static String yyyymmdd_hh_mm_a = "yyyy-MM-dd hh:mm a";
-}
