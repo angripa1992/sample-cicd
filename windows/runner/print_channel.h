9a2195f2f8cdb4c89c09939d3c46566f3494aaa0
angripa.nadapdap <angripa.nadapdap@klikit.io>
2025-05-20 16:42:36 +0700

    update usb code printing

--- windows/runner/print_channel.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINT_CHANNEL_H_
-#define PRINT_CHANNEL_H_
-
-#include <flutter/flutter_view_controller.h>
-
-void RegisterPrintMethodChannel(flutter::FlutterViewController* controller);
-
-#endif  // PRINT_CHANNEL_H_
